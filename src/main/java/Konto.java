2c949afded137a1a341ed3dcd4ada67d607c09bc
Malte <maltebjoerklund@gmail.com>
2021-10-26 16:31:14 +0200

    Fjernelse af Display i Konto

--- src/main/java/Konto.java
+++ src/main/java/Konto.java
@@ -1,12 +1,6 @@
 public class Konto {
     int pengeTotal;
 
-    public Konto() {pengeTotal = pengeTotal;}
-
-    void display() {
-        System.out.println(pengeTotal);
-    }
-
     public void addPoint(int penge){
         pengeTotal += penge;
     }
