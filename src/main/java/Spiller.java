dd77b5d63e8d35c43e80a9d21b40a42c78c64552
malte <maltebjoerklund@gmail.com>
2021-10-28 00:35:37 +0200

    Små ændringer

--- src/main/java/Spiller.java
+++ src/main/java/Spiller.java
@@ -3,19 +3,15 @@ public class Spiller {
 
     Konto konto = new Konto();
 
-// mangler Declaration of state/Properties.
-// mangler en constructor " objekt"
-int startPoint ;
+    int startPoint ;
 
     public void setStartPoint(int startPoint) {
         this.startPoint = startPoint;
         konto.addPoint(startPoint);
     }
-    // Declaration of Actions " behavior"
+
     public Spiller(){konto.addPoint(startPoint);}
 
-    // Declaration of Actions.
-    // Instance method.
     public void addToTotal(int points) {
         konto.addPoint(points);
     }
