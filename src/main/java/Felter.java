fd82847b88db7889af923edf875f8c0486c36242
malte <maltebjoerklund@gmail.com>
2021-10-29 11:17:46 +0200

    Slettede addToTotal

--- src/main/java/Felter.java
+++ src/main/java/Felter.java
@@ -8,35 +8,35 @@ public class Felter{
 
         //"Bræt" med egenskaber
             switch(felt){ // Terningernes sum der afgører placeringen på brættet
-                case 2: spiller.addToTotal(250);
+                case 2: spiller.konto.addPoint(250);
                     feltBeskrivelse = "\tTårn \t\t+250\n" +
                             "Du er havnet i et tårn med en masse værdifulde genstande! \n" +
                             "Ingen ville lægge mærke til at der manglede noget… \uD83D\uDE42 \n" +
                             "Du har tjent 250kr.";
                     break;
 
-                case 3: spiller.addToTotal(-100);
+                case 3: spiller.konto.addPoint(-100);
                     feltBeskrivelse = "Krater \t\t-100\n" +
                             "WOOOOW du faldt ned i et krater!!!! \n" +
                             "Heldigvis er der nogen til at hjælpe dig op. \n" +
                             "Det krævede dog 100 kr at få sat dem igang.";
                     break;
 
-                case 4: spiller.addToTotal(100);
+                case 4: spiller.konto.addPoint(100);
                     feltBeskrivelse = "Templets Porte\t\t+100\n" +
                             "Et kæmpe palads er for øjnene af dig. \n" +
                             "Du står ved portene, men bliver ikke lukket ind. \n" +
                             "På vej væk derfra finder du dog 100kr!";
                     break;
 
-                case 5: spiller.addToTotal(-20);
+                case 5: spiller.konto.addPoint(-20);
                     feltBeskrivelse = "Den Kolde Ørken\t \t-20\n" +
                             "Du er strandet i den kolde ørken. \n" +
                             "I desperation efter mad og drikke,\n" +
                             "slugte du en 20'er.";
                     break;
 
-                case 6: spiller.addToTotal(180);
+                case 6: spiller.konto.addPoint(180);
                     feltBeskrivelse = "Den Befæstede By\t\t+180 \n" +
                             "Du tog et job i den befæstede by og tjente 180.";
                     break;
@@ -51,11 +51,11 @@ public class Felter{
                     feltBeskrivelse = "Den Sorte Hule \t\t-70";
                     break;
 
-                case 9: spiller.addToTotal(60);
+                case 9: spiller.konto.addPoint(60);
                     feltBeskrivelse = "Bjerghytterne\t\t+60";
                     break;
 
-                case 10: spiller.addToTotal(-80);
+                case 10: spiller.konto.addPoint(-80);
                     feltBeskrivelse = "Varulv-Muren        -80\n" +
                             "Ved en lang og høj mur hører du hylen i det fjerne… \n" +
                             "DU ER HAVNET VED VAREULV-MUREN!!! \n" +
@@ -64,13 +64,13 @@ public class Felter{
                             "Skynd dig at kaste igen!";
                     break;
 
-                case 11: spiller.addToTotal(-50);
+                case 11: spiller.konto.addPoint(-50);
                     feltBeskrivelse = "Hullet\t\t-50\n" +
                             "Du kom til at kigge op mens du gik og nu faldt du ned i et hul… \n" +
                             "Du tabte en 50'er.";
                     break;
 
-                case 12: spiller.addToTotal(650);
+                case 12: spiller.konto.addPoint(650);
                     feltBeskrivelse = "Guldminen\t\t+650 \n" +
                             "JACKPOT! Du fandt en guldmine! \n" +
                             "Du fylder lommerne og en masse andet med guld\n" +
